8c508e46fbd0caa0e07b70344cf6220c64669e85
Tyler Long <tyler4long@gmail.com>
2017-01-17 16:51:16 +0800

    First working version

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ load_dotenv(find_dotenv())
 
 rc = RestClient(os.environ.get('appKey'), os.environ.get('appSecret'), os.environ.get('server'))
 rc.authorize(os.environ.get('username'), os.environ.get('extension'), os.environ.get('password'))
+
+r = rc.get('/restapi/v1.0/dictionary/country/46')
+print r.text
--- rest_client.py
+++ rest_client.py
@@ -1,7 +1,39 @@
+import urlparse
+import requests
+import base64
+
 class RestClient(object):
     def __init__(self, appKey, appSecret, server):
         self.appKey = appKey
         self.appSecret = appSecret
         self.server = server
+        self.token = None
+
     def authorize(self, username, extension, password):
-        print(username)
+        url = urlparse.urljoin(self.server, '/restapi/oauth/token')
+        data = {
+            'username': username,
+            'extension': extension,
+            'password': password,
+            'grant_type': 'password'
+        }
+        headers = {
+            'Authorization': self._autorization_header()
+        }
+        r = requests.post(url, data = data, headers = headers)
+        self.token = r.json()
+
+    def get(self, endpoint, params = None):
+        url = urlparse.urljoin(self.server, endpoint)
+        headers = {
+            'Authorization': self._autorization_header()
+        }
+        return requests.get(url, params = params, headers = headers)
+
+    def _autorization_header(self):
+        if self.token:
+            return 'Bearer {access_token}'.format(access_token = self.token['access_token'])
+        return 'Basic {basic_key}'.format(basic_key = self._basic_key())
+
+    def _basic_key(self):
+        return base64.b64encode('{appKey}:{appSecret}'.format(appKey = self.appKey, appSecret = self.appSecret))
