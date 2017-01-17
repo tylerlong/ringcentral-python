dda49951ef637be1214e43194808b270e0714ac4
Tyler Long <tyler4long@gmail.com>
2017-01-17 17:17:53 +0800

    Create env.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from dotenv import load_dotenv, find_dotenv
-import os
 from rest_client import RestClient
+from env import appKey, appSecret, server, username, extension, password
 
-load_dotenv(find_dotenv())
-
-rc = RestClient(os.environ.get('appKey'), os.environ.get('appSecret'), os.environ.get('server'))
-rc.authorize(os.environ.get('username'), os.environ.get('extension'), os.environ.get('password'))
+rc = RestClient(appKey, appSecret, server)
+rc.authorize(username, extension, password)
 
 r = rc.get('/restapi/v1.0/dictionary/country/46')
 print r.text
