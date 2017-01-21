b1901fccfdfd749c47aa45cda23409d57c109224
Tyler Long <tyler4long@gmail.com>
2017-01-21 12:06:37 +0800

    Create test/subscription.py

--- test/index.py
+++ test/index.py
@@ -2,7 +2,6 @@ import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
 
-import time
 from rest_client import RestClient
 from env import appKey, appSecret, server, username, extension, password, receiver
 
@@ -37,26 +36,5 @@ def test_CRUD():
     r = rc.delete('/restapi/v1.0/account/~/extension/~/message-store/{message_id}'.format(message_id = message_id), { 'purge': False })
     print r.status_code
 
-def test_PubNub():
-    def message_callback(json):
-        print json
 
-    # subscribe
-    events = ['/restapi/v1.0/account/~/extension/~/message-store']
-    subscription = rc.subscription(events, message_callback)
-    subscription.subscribe()
-
-    # send an SMS to tigger a notification
-    data = {
-        'from': { 'phoneNumber': username },
-        'to': [{ 'phoneNumber': receiver }],
-        'text': "hello world"
-    }
-    r = rc.post('/restapi/v1.0/account/~/extension/~/sms', data)
-
-    # wait for the notification to come
-    time.sleep(20)
-
-
-# test_CRUD()
-test_PubNub()
+test_CRUD()
