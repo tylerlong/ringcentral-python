55ee602444480712b4e4c7cb8231c920a5235db9
Tyler Long <tyler4long@gmail.com>
2017-01-23 10:00:26 +0800

    Add a sample setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ringcentral_client',
+    version='0.1.0',
+    description='Python Client for RingCentral API',
+    author='Tyler Long',
+    author_email='tyler.liu@ringcentral.com',
+    url='https://github.com/tylerlong/ringcentral-python',
+    packages=find_packages(exclude=("test*",)),
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    install_requires=[
+        'requests>=2.12.5',
+        'pubnub>=4.0.6',
+        'pycrypto>=2.6.1',
+    ],
+)
