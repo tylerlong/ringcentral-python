2ffb5f4e4af608541d7b7e24b61220e8c852441b
Tyler Long <tyler4long@gmail.com>
2017-01-25 16:23:10 +0800

    Pass fax testing

--- test/test_fax.py
+++ test/test_fax.py
@@ -4,10 +4,12 @@ import os
 
 class FaxTestCase(BaseTestCase):
     def test_send_fax(self):
+        image_file = open(os.path.join(os.path.dirname(__file__), 'test.png'), 'rb')
         files = [
             ('json', ('request.json', json.dumps({ 'to': [{ 'phoneNumber': self.receiver }] }), 'application/json')),
             ('attachment', ('test.txt', 'Hello world', 'text/plain')),
-            ('attachment', ('test.png', open(os.path.join(os.path.dirname(__file__), 'test.png'), 'rb'), 'image/png')),
+            ('attachment', ('test.png', image_file, 'image/png')),
         ]
         r = self.rc.post('/restapi/v1.0/account/~/extension/~/fax', files = files)
         self.assertEqual(200, r.status_code)
+        image_file.close()
