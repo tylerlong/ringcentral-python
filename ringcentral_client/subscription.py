d488fa3b6dc0d2c09c12b89a4412c04e38ad2cb5
Tyler Long <tyler4long@gmail.com>
2017-03-03 14:13:56 +0800

    Revoke pubnub properly

--- ringcentral_client/subscription.py
+++ ringcentral_client/subscription.py
@@ -54,7 +54,12 @@ class Subscription(object):
         self.subscription = r.json()
 
     def revoke(self):
+        if not self.subscription:
+            return
         self.pubnub.remove_listener(self.callback)
+        self.pubnub.unsubscribe().channels(self.subscription['deliveryMode']['address']).execute()
+        self.pubnub.revoke()
+        self.pubnub = None
         self.rc.delete('/restapi/v1.0/subscription/{id}'.format(id = self.subscription['id']))
         self.subscription = None
 
