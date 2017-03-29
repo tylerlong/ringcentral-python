243ce3a23647a2798026e86e181535980b874d84
Tyler Long <tyler4long@gmail.com>
2017-03-29 11:15:10 +0800

    Fix Python 3 syntax issue

--- test/test_meetings.py
+++ test/test_meetings.py
@@ -4,6 +4,6 @@ class MeetingsTestCase(BaseTestCase):
     def test_get_meetings_list(self):
         try:
             self.rc.get('/restapi/v1.0/account/~/extension/~/meeting')
-        except Exception, e:
+        except Exception as e:
             self.assertTrue('permission required' in str(e))
             self.assertTrue('403' in str(e))
