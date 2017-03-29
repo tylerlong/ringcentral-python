b80b597bc1f41902f49a7d3a10a689e2c23e01dc
Tyler Long <tyler4long@gmail.com>
2017-03-29 11:29:44 +0800

    Assert meetings response

--- test/test_meetings.py
+++ test/test_meetings.py
@@ -3,7 +3,8 @@ from .test_base import BaseTestCase
 class MeetingsTestCase(BaseTestCase):
     def test_get_meetings_list(self):
         try:
-            self.rc.get('/restapi/v1.0/account/~/extension/~/meeting')
+            r = self.rc.get('/restapi/v1.0/account/~/extension/~/meeting')
+            self.assertEqual(200, r.status_code)
         except Exception as e:
             self.assertTrue('permission required' in str(e))
             self.assertTrue('403' in str(e))
