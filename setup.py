22c794c475f94bb5594d3c3216323b76aa812ff0
Tyler Long <tyler4long@gmail.com>
2017-06-30 14:21:34 +0800

    Release 0.2.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ringcentral_client',
-    version='0.2.5',
+    version='0.2.6',
     description='Python Client for RingCentral API',
     author='Tyler Long',
     author_email='tyler.liu@ringcentral.com',
