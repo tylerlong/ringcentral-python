2c615689ba4fff0108bf5402aa1697052254d159
Tyler Long <tyler4long@gmail.com>
2018-03-07 17:17:49 +0800

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ringcentral_client',
-    version='0.2.7',
+    version='0.3.0',
     description='Python Client for RingCentral API',
     author='Tyler Long',
     author_email='tyler.liu@ringcentral.com',
@@ -10,8 +10,8 @@ setup(
     packages=find_packages(exclude=('test*',)),
     license='MIT',
     install_requires=[
-        'requests>=2.13.0',
-        'pubnub>=4.0.10',
+        'requests>=2.18.4',
+        'pubnub>=4.0.13',
         'pycrypto>=2.6.1',
     ],
 )
