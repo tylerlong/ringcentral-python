9fe68854caccaf521893c863badded30565457e1
Tyler Long <tyler4long@gmail.com>
2018-06-01 10:11:06 +0800

    404 message changed, update test case

--- test/test_exception.py
+++ test/test_exception.py
@@ -5,7 +5,7 @@ import os
 class ExceptionTestCase(BaseTestCase):
     def test_404(self):
         try:
-            r = self.rc.get('/restapi/v1.0/account/~/extension/~/does-not-exist')
+            self.rc.get('/restapi/v1.0/account/~/extension/~/does-not-exist')
         except Exception as e:
-            self.assertTrue('Invalid URI' in str(e))
+            self.assertTrue('Resource not found' in str(e))
             self.assertTrue('404' in str(e))
