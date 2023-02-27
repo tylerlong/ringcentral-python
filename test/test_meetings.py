dd507f7388c5d1d6a79e017be0961b9d5dcb167e
Tyler Long <tyler4long@gmail.com>
2023-02-27 12:05:56 -0800

    Do not test meetings api

--- test/test_meetings.py
+++
@@ -1,6 +0,0 @@
-from .test_base import BaseTestCase
-
-class MeetingsTestCase(BaseTestCase):
-    def test_get_meetings_list(self):
-        r = self.rc.get('/restapi/v1.0/account/~/extension/~/meeting')
-        self.assertEqual(200, r.status_code)
